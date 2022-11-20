08e4c6133ac00d7b5d105429e3c5aa3acacf81ba
craftyboi <craftyboi@pop-os.localdomain>
2022-11-20 14:33:49 +0530

    Auth_context_error cleared

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,27 +1,37 @@
-import { createContext, useContext } from 'react'
-import { createUserWithEmailAndPassword, 
-    // signInWithEmailAndPassword, 
-    // signOut, 
-    // onAuthStateChanged
-} from 'firebase/auth'
-import {auth} from '../firebase'
+import { createContext, useContext, useEffect, useState } from 'react';
+import {
+  createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+//   signOut,
+//   onAuthStateChanged,
+} from 'firebase/auth';
+import { auth } from '../firebase';
 
-const UserContext = createContext()
+const UserContext = createContext();
 
-export const AuthContextProvider = ({children}) => {
+export const AuthContextProvider = ({ children }) => {
+  
 
-    const createUser = (email, password) => {
-        return createUserWithEmailAndPassword(auth,email, password)
-    }
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
+ 
 
-    return(
-        <UserContext.Provider value={ createUser }>
-            {children}
-        </UserContext.Provider>
-    )
+ 
+
+ 
+  return (
+    <UserContext.Provider value={{ createUser  }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const UserAuth = () => {
+  return useContext(UserContext);
 }
 
-export const UserAuth = ( ) => {
-    return useContext(UserContext)
-}
+
+ 
+
